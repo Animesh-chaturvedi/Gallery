f95cc7cad2230087430def8dd5faa8d1bbdbef2f
Animesh chaturvedi <animesh.chaturvedi.1234@gmail.com>
2019-04-30 00:47:42 +0530

    final commit

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var multer = require("multer");
 var fs = require("fs-extra");
+var path = require("path");
 var port = 3000;
 var ip = "localhost";
+app.use(express.static("./"))
+var Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "uploads/");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  }
+});
 
-var upload = multer({ dest: "uploads/" });
+var upload = multer({
+  storage: Storage
+});
 
 mongoose.connect("mongodb://localhost:27017/images_db", {
   useNewUrlParser: true
@@ -15,7 +27,7 @@ mongoose.connect("mongodb://localhost:27017/images_db", {
 
 var imagesSchema = new mongoose.Schema({
   name: String,
-  image: { data: Buffer, contentType: String }
+  image: String
 });
 
 var Image = mongoose.model("Image", imagesSchema);
@@ -23,8 +35,8 @@ var Image = mongoose.model("Image", imagesSchema);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-  Image.find({}, function(err, allImages) {
+app.get("/", function (req, res) {
+  Image.find({}, function (err, allImages) {
     if (err) {
       console.log(err);
     } else {
@@ -33,24 +45,19 @@ app.get("/", function(req, res) {
   });
 });
 
-app.post("/", upload.single("photo"), function(req, res) {
-  var a = new Image();
-  a.name = req.body.name;
-  var img = fs.readFileSync(req.file.path);
-  a.image.data = img.toString("base64");
-  a.image.contentType = "image/jpg";
-  a.save(function(err, a) {
-    if (err) throw err;
-
-    console.error("saved img to mongo");
+app.post("/", upload.single("photo"), function (req, res) {
+  var name = req.body.name;
+  var image = req.file.filename;
+  var newimage = { name: name, image: image };
+  Image.create(newimage, function () {
     res.redirect("/");
   });
 });
 
-app.get("/new", function(req, res) {
+app.get("/new", function (req, res) {
   res.render("new.ejs");
 });
 
-app.listen(port, ip, function() {
+app.listen(port, ip, function () {
   console.log("Gallery");
 });
